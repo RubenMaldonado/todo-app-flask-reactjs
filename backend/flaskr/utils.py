c2c9c20bb9487a4da116d4c7fa8b984d93879fef
Ruben Maldonado <ruben@maldonadotena.com>
2025-07-29 12:26:44 -0400

    Fix hashing passwerd method for compability

--- backend/flaskr/utils.py
+++ backend/flaskr/utils.py
@@ -2,7 +2,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 def generate_password(password):
-    return generate_password_hash(password, salt_length=10)
+    # Use pbkdf2:sha256 which is widely supported
+    return generate_password_hash(password, method='pbkdf2:sha256', salt_length=10)
 
 
 def check_password(password_hash, password):
